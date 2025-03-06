0c7d4b4aa74830139442ef868edde74c1507a160
bro <dota22papich@mail.ru>
2025-03-06 23:04:22 +0300

    fix

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
 
-export const greetUser = () => {
+const greetUser = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
 };
+
+export default greetUser;

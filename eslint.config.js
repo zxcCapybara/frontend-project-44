82bd439f69b74b693f4abedb7719337d6578c082
bro <dota22papich@mail.ru>
2025-03-06 23:33:55 +0300

    fix4

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 
-/** @type {import('eslint').Linter.Config[]} */
+/** @type { import('eslint').Linter.Config[] } */
 export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
